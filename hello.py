a12b0be069718bfcd74a6191605286c724476ff9
qweraass <qweraass@gmail.com>
2025-11-12 20:05:35 +0900

    Modify: Update print

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ PORT = 1101
 
 try:
     
+    print("jobpy => hello.py => GitHub ")
     print(f"{IP}:{PORT}")
     so = socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(10)
